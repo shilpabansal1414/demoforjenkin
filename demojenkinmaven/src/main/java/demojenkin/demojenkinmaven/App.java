5d125dec571fa9d2d2f94c857a91f2142e87c64d
lalit <lalit@10.0.0.102>
2021-02-05 02:55:13 -0600

    update

---
+++ demojenkinmaven/src/main/java/demojenkin/demojenkinmaven/App.java
@@ -0,0 +1,13 @@
+package demojenkin.demojenkinmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
