1744ca0b5a12c7faaeae407cae802d8f003ca1fc
lalit <lalit@10.0.0.102>
2021-02-05 02:34:44 -0600

    first demo code for jenkin

---
+++ demojenkinmaven/src/main/java/demojenkin/demojenkinmaven/class1.java
@@ -0,0 +1,15 @@
+package demojenkin.demojenkinmaven;
+
+import org.testng.annotations.Test;
+
+public class class1 
+{
+  @Test
+	public void display()
+	{
+		System.out.println("Ist test print");
+		System.out.println("2nd test print");
+		System.out.println("3rd test print");
+		System.out.println("4th test print");
+	}
+}
