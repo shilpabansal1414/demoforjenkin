f8022e86c39d3050c75d4ba5b2d565179116119a
lalit <lalit@10.0.0.102>
2021-02-05 22:43:43 -0600

    update printing command

--- demojenkinmaven/src/main/java/demojenkin/demojenkinmaven/class1.java
+++ demojenkinmaven/src/main/java/demojenkin/demojenkinmaven/class1.java
@@ -11,5 +11,6 @@ public class class1
 		System.out.println("2nd test print");
 		System.out.println("3rd test print");
 		System.out.println("4th test print");
+		System.out.println("Added new line");
 	}
 }
